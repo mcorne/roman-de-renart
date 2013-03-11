8a138bc058ddaf3f3f5d482a550bbbb1fd89d712
miccorne@gmail.com <miccorne@gmail.com@5e6d9301-82ee-3f6c-f07d-548891fc056a>
2013-03-11 12:57:34 +0000

    added episode
    git-svn-id: https://roman-de-renart.googlecode.com/svn/trunk@60 5e6d9301-82ee-3f6c-f07d-548891fc056a

--- application/common.php
+++ application/common.php
@@ -12,7 +12,7 @@
 
 define('INDEX_ROWS', 1);
 
-define('MAX_VERSES_TO_TRANSLATE', 100);
+define('MAX_VERSES_TO_TRANSLATE', 120);
 
 // the list of punctuation characters to be used in a preg function pattern
 define('PUNCTUATION', '!|,|\.|:|;|\?|«|»|—|“|”');
